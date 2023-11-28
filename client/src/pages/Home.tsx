0776dbb35fb507dc22d2df9efa789234eaf9409d
Andelo Jakolis <andelo.jakolis@gmail.com>
2023-11-28 14:21:45 +0100

    added some random text to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
-    <div>
-      <h1 className="font-epilogue text-[#5cbdb9] text-[40px]">Welcome, to our Lorem ipsum restaurant</h1>
-      <p className="font-epilogue">please Login to book your table</p>
+    <div className="mt-32">
+      <h1 className="font-epilogue text-[#5cbdb9] text-[40px] text-center">Welcome, to our Lorem ipsum restaurant</h1>
+      <p className="font-epilogue text-[20px] text-center">please Login to book your table</p>
     </div>
   )
 }
