95c4697720695fd23399c14c45f6996303316b0c
Andelo Jakolis <andelo.jakolis@gmail.com>
2023-12-12 15:33:37 +0100

    finished table booking basic frontend

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,25 +1,51 @@
 import React from 'react'
 import { table } from '../assets';
+import { tableStyles } from '../constants';
+
+// interface TableProps {
+//     imageNumber: number;
+//     handleImageClick: (imageNumber: number) => void;
+//     opacity: boolean;
+// }
+
+// const Table: React.FC<TableProps> = ({imageNumber, handleImageClick, opacity}) => {
+//   return (
+//     <div className={`sm:ml-8 ml-2 basis-1/3 z-10`}>
+//         <img
+//             onClick={() => handleImageClick(imageNumber)}
+//             className={`${opacity ? 'opacity-75 cursor-pointer' : 'opacity-25'}`}
+//             src={table}
+//             alt={`Table image ${imageNumber}`}
+//             width={200}
+//             height={100}
+//         />
+//     </div>
+//   )
+// }
 
 interface TableProps {
-    imageNumber: number;
-    handleImageClick: (imageNumber: number) => void;
-    opacity: boolean;
+  index: number;
+  handleImageClick: (index: number) => void;
+  isReserved: boolean;
 }
 
-const Table: React.FC<TableProps> = ({imageNumber, handleImageClick, opacity}) => {
+const Table: React.FC<TableProps> = ({ index, handleImageClick, isReserved }) => {
+  
+  const handleClick = () => {
+    if (!isReserved) {
+      handleImageClick(index + 1);
+    }
+  };
+
   return (
-    <div className={`sm:ml-8 ml-2 basis-1/3 z-10`}>
-        <img
-            onClick={() => handleImageClick(imageNumber)}
-            className={`${opacity ? 'opacity-75 cursor-pointer' : 'opacity-25'}`}
-            src={table}
-            alt={`Table image ${imageNumber}`}
-            width={200}
-            height={100}
-        />
+    <div className={tableStyles[index]} onClick={handleClick}>
+      <img
+        className={`${isReserved ? 'opacity-25' : 'opacity-75 cursor-pointer'} sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]`}
+        src={table}
+        alt="Table image"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Table
--- client/src/pages/TableBooking.tsx
+++ client/src/pages/TableBooking.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Table from '../components/Table';
-import { restaurant, table } from '../assets';
+import { restaurant } from '../assets';
 
 const TableBooking: React.FC = () => {
 
@@ -11,6 +11,8 @@ const TableBooking: React.FC = () => {
   const [time, setTime] = useState<string>('');
   const [persons, setPersons] = useState<number>(1);
   const [mealType, setMealType] = useState<string>('lunch');
+  const [filterDate, setFilterDate] = useState('');
+  const [filterMealType, setFilterMealType] = useState('');
 
   const openModal = (index: number) => {
     setSelectedImageIndex(index);
@@ -46,68 +48,65 @@ const TableBooking: React.FC = () => {
     closeModal();
   };
 
-  const fetchedArray = [1,6,8]
+  useEffect(() => {
+    const now = new Date();
+    const hours = now.getHours();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const defaultDate = `${year}-${month}-${day}`;
+    setFilterDate(defaultDate);
+
+    if (hours >= 15 && hours < 24) {
+      setFilterMealType('dinner');
+    } else {
+      setFilterMealType('lunch');
+    }
+  }, []);
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterDate(event.target.value);
+  };
+
+  const handleMealTypeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterMealType(event.target.value);
+  };
+
+  const handleFilterSubmit = () => {
+    // send to backend
+    console.log({ filterDate, filterMealType });
+  };
+
+  const fetch = [7,8,9,10]
 
   return (
     <div className='flex sm:flex-row flex-col relative'>
       <div className='relative w-[90vw]'>
-        <img className='sm:h-[80vh] h-[50vh] m-auto sm:w-[60%]  z-0 rounded-lg' src={restaurant} alt="Restaurant Floor Plan" />
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute top-[30%] left-[45%]'><img onClick={() => handleImageClick(1)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute top-[30%] sm:left-[55%] left-[60%]'><img onClick={() => handleImageClick(2)} className='opacity-25 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute top-[30%] sm:left-[65%] left-[75%]'><img onClick={() => handleImageClick(3)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute top-[45%] sm:left-[50%] left-[55%]'><img onClick={() => handleImageClick(4)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute top-[45%] sm:left-[60%] left-[70%]'><img onClick={() => handleImageClick(5)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute bottom-[20%] sm:left-[50%] left-[55%]'><img onClick={() => handleImageClick(6)} className='opacity-25 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute bottom-[20%] sm:left-[60%] left-[70%]'><img onClick={() => handleImageClick(7)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute bottom-[3%] left-[45%]'><img onClick={() => handleImageClick(8)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute bottom-[3%] sm:left-[54%] left-[59%]'><img onClick={() => handleImageClick(9)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3 z-10 absolute bottom-[3%] sm:left-[63%] left-[72%]'><img onClick={() => handleImageClick(10)} className='cursor-pointer opacity-75 sm:w-[65px] sm:h-[65px] w-[30px] h-[30px]' src={table} alt="Table image"/></div>
-      </div>
-      <div className='font-epilogue sm:w-[19vw] w-[90vw] h-[22vh] absolute sm:left-[20%] left-[-4px] sm:bottom-[1px] bottom-[-50%] rounded-lg text-left p-4'>
-        <label htmlFor="">Date: </label>
-        <input type="date" name="" id="" /><br />
-        <label htmlFor="lunch">Lunch </label> <input type="radio" name="mealType" id="lunch" /><br />
-        <label htmlFor="dinner">Dinner </label> <input type="radio" name="mealType" id="dinner" /><br />
-        <button className='font-epilogue font-semibold text-[16px] leading-[26px] text-[white] min-h-[52px] px-4 rounded-[10px] border border-solid border-[white] sm:ml-11 ml-7 mt-2 bg-[#5cbdb9]' type="submit">Check availability</button>
-      </div> 
-    {/*<div className='flex flex-col'>
-      {[1, 2, 3].map((row) => (
-        <div key={row} className='flex flex-row justify-normal gap-2'>
-          {[1, 2, 3].map((col) => {
-            const imageNumber = row * 3 + col - 3;
-            const opacity = !fetchedArray.includes(imageNumber);
-            const handleClick = opacity ? () => handleImageClick(imageNumber) : () => {};
-
-            return (
-              <Table 
-                key={col}
-                imageNumber={imageNumber}
-                handleImageClick={handleClick}
-                opacity={opacity}
-              />
-            )
-          })}
-        </div>
+      <img className='sm:h-[80vh] h-[50vh] m-auto sm:w-[60%]  z-0 rounded-lg' src={restaurant} alt="Restaurant Floor Plan" />
+      {[...Array(10).keys()].map((index) => (
+        <Table
+          key={index}
+          index={index}
+          handleImageClick={handleImageClick}
+          isReserved={fetch.includes(index + 1)}
+        />
       ))}
-       <div className='flex flex-row justify-normal gap-2'>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(1)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(2)} className='opacity-25' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(3)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-      </div>
-
-      <div className='flex flex-row justify-normal gap-2'>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(4)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(5)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(6)} className='opacity-25' src={table} alt="Table image" width={200} height={100}/></div>
-      </div>
-
-      <div className='flex flex-row justify-normal gap-2'>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(7)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(8)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-        <div className='sm:ml-8 ml-2 basis-1/3'><img onClick={() => handleImageClick(9)} className='cursor-pointer opacity-75' src={table} alt="Table image" width={200} height={100}/></div>
-      </div> 
-</div>*/}
-<div>{/** kalendar i rucak/vecera */}</div>
+    </div>
+      <div className='font-epilogue sm:w-[19vw] w-[90vw] h-[22vh] absolute sm:left-[20%] left-[-4px] sm:bottom-[1px] bottom-[-50%] rounded-lg text-left p-4'>
+      <label htmlFor="">Date: </label>
+      <input type="date" name="" id="" value={filterDate} onChange={handleDateChange} /><br />
+      <label htmlFor="lunch">Lunch </label>
+      <input type="radio" name="mealType" id="lunch" value="lunch" checked={filterMealType === 'lunch'} onChange={handleMealTypeChange} /><br />
+      <label htmlFor="dinner">Dinner </label>
+      <input type="radio" name="mealType" id="dinner" value="dinner" checked={filterMealType === 'dinner'} onChange={handleMealTypeChange} /><br />
+      <button
+        className='font-epilogue font-semibold text-[16px] leading-[26px] text-[white] min-h-[52px] px-4 rounded-[10px] border border-solid border-[white] sm:ml-11 ml-7 mt-2 bg-[#5cbdb9]'
+        type="submit"
+        onClick={handleFilterSubmit}
+      >
+        Check availability
+      </button>
+    </div>
       {isModalOpen && (
         <div onClick={handleModalClick} className='z-20 fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
           <div className='bg-white p-8 rounded shadow-lg'>
