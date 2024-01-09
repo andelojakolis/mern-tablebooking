3dd8f9afdfd721a65968c2225ac85488d4a7385b
Andelo Jakolis <andelo.jakolis@gmail.com>
2024-01-09 14:47:44 +0100

    Homepage WIP: styled divisions for small screen and lg

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,17 @@
 const Home = () => {
+
+  const token = localStorage.getItem("accessToken");
+
   return (
-    <div className="mt-32">
-      <h1 className="font-epilogue text-[#5cbdb9] text-[40px] text-center">Welcome, to our Lorem ipsum restaurant</h1>
-      <p className="font-epilogue text-[20px] text-center">please Login to book your table</p>
+    <div className="flex md:flex-row flex-col font-epilogue mx-3 w-[80vw] h-[80vh]">
+      <div className="md:w-[40%] w-[100%] h-[100%] bg-[white] m-2 rounded-lg border border-solid border-[#5cbdb9]">
+        <h1 className="font-epilogue font-bold text-[#5cbdb9] md:text-[40px] text-[20px] text-center m-2">Welcome,</h1>
+        <p className="font-epilogue md:text-[20px] text-[14px] text-center m-2">{token ? 'to our restaurant dear (user.name), you can book your table or leave a review' : 'dear guest, please login or register'}</p>
+      </div>
+
+      <div className="md:w-[60%] w-[100%] h-[100%] bg-[white] m-2 rounded-lg border border-solid border-[#5cbdb9]">
+
+      </div>
     </div>
   )
 }
